1bd6a4403749978866aa4d79d742276c9281bd94
your_name <your_email>
2026-05-13 14:26:23 +0530

    Removed duplicate Google app config

--- myapp/settings.py
+++ myapp/settings.py
@@ -95,10 +95,7 @@ SOCIALACCOUNT_PROVIDERS = {
     "google": {
         "SCOPE": ["profile", "email"],
         "AUTH_PARAMS": {"access_type": "online"},
-        "APP": {
-            "client_id": os.getenv("GOOGLE_CLIENT_ID"),
-            "secret": os.getenv("GOOGLE_CLIENT_SECRET"),
-        }
+        
     }
 }
 
